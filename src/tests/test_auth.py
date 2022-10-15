3cfd5e50c7278819fd11587dbdc2faf4d242ef0b
Arif_Gafizov <agafizov@gmail.com>
2022-10-15 19:35:35 +0300

    Added test refresh token

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -1,10 +1,26 @@
 import pytest
 
 
+@pytest.fixture(scope='module')
+def credentials():
+    return {"email": "test@example.com", "password": "supertest"}
+
+
 @pytest.mark.anyio
-async def test_login(app_client):
-    response = await app_client.post("/login", json={"email": "test@example.com", "password": "supertest"})
+async def test_login(app_client, credentials):
+    response = await app_client.post("/login", json=credentials)
     response_data = response.json()
     assert response.status_code == 200
+    assert "access_token" in response_data
+    assert "refresh_token" in response_data
+
+
+@pytest.mark.anyio
+async def test_refresh_token(app_client, credentials):
+    response = await app_client.post("/login", json=credentials)
+    refresh_token = response.json()['refresh_token']
+    response = await app_client.post("/token/refresh", json={'refresh': refresh_token})
+    assert response.status_code == 200
+    response_data = response.json()
     assert "access_token" in response_data
     assert "refresh_token" in response_data
