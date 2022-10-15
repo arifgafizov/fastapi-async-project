c5ba2b3440eeeb7bc58297324d9344279818cf81
Arif_Gafizov <agafizov@gmail.com>
2022-10-15 20:09:15 +0300

    Added test_list_users

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -28,3 +28,11 @@ async def test_create_user(app_client, user_data):
     assert response_data["profile"]["last_name"] == user_data["profile"]["last_name"]
     assert response_data["profile"]["bio"] == user_data["profile"]["bio"]
     assert response_data["profile"]["is_active"]
+
+
+@pytest.mark.anyio
+async def test_list_users(app_client, auth_header):
+    response = await app_client.get("/users", headers=auth_header)
+    response_data = response.json()
+    assert response.status_code == 200
+    assert response_data
