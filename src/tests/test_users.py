2e307c701d135972bdc2b252e611be6513c53c62
Arif_Gafizov <agafizov@gmail.com>
2022-10-15 20:37:53 +0300

    Added test_retrieve_user

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -17,7 +17,7 @@ def user_data():
 
 
 @pytest.mark.anyio
-async def test_create_user(app_client, user_data):
+async def test_create_user(app_client, user_data, request):
     response = await app_client.post("/register", json=user_data)
     response_data = response.json()
     assert response.status_code == 201
@@ -28,11 +28,21 @@ async def test_create_user(app_client, user_data):
     assert response_data["profile"]["last_name"] == user_data["profile"]["last_name"]
     assert response_data["profile"]["bio"] == user_data["profile"]["bio"]
     assert response_data["profile"]["is_active"]
+    request.config.cache.set('user_id', response_data["id"])
 
 
 @pytest.mark.anyio
 async def test_list_users(app_client, auth_header):
     response = await app_client.get("/users", headers=auth_header)
+    response_data = response.json()
+    assert response.status_code == 200
+    assert response_data
+
+
+@pytest.mark.anyio
+async def test_retrieve_user(app_client, auth_header, request):
+    user_id = request.config.cache.get('user_id', None)
+    response = await app_client.get(f"/users/{user_id}", headers=auth_header)
     response_data = response.json()
     assert response.status_code == 200
     assert response_data
